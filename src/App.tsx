dd2249e7a153fe3320620663bb645de04ea6a227
Dusan Stanic <dusan.stanic97@hotmail.com>
2025-03-30 17:52:34 +0200

    Add contact me

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { ThemeProvider } from "./features/theme/context";
 import { Landing } from "./features/landing/page/Landing/Landing";
 
 import { ToggleButton } from "./features/theme/components/ToggleButton/ToggleButton";
+import { Route, Routes } from "react-router";
 
 export const App = () => {
   return (
     <ThemeProvider>
       <>
-        <Landing />
+        <Routes>
+          <Route index element={<Landing />} />
+        </Routes>
         <ToggleButton />
       </>
     </ThemeProvider>
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 
 import { createRoot } from "react-dom/client";
 
+import { BrowserRouter } from "react-router";
+
 import { App } from "./App.tsx";
 
 import "@/styles/variables.scss";
@@ -11,6 +13,8 @@ import "./index.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
