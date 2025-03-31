938ca580bcbdcf54f856025709bdcfd631f9b76d
Dusan Stanic <dusan.stanic97@hotmail.com>
2025-03-31 22:04:40 +0200

    Update About Me Section

--- src/features/landing/page/AboutMe/AboutMe.tsx
+++ src/features/landing/page/AboutMe/AboutMe.tsx
@@ -7,15 +7,17 @@ interface IAboutMeProps {
   text: string;
 }
 
+console.log(classes);
+
 export const AboutMe = ({ title, text }: IAboutMeProps) => {
   return (
     <div className={classes.aboutMe} id="aboutMe">
-      <div>
-        <h3>{title}</h3>
+      <div className={classes.headingContainer}>
+        <h3 className={classes.heading}>{title}</h3>
       </div>
       <div>
         <div className={classes.imageWrapper}>
-          <img alt="User Image" src={image} />
+          <img alt="User Image" src={image} className={classes["fade-img"]} />
         </div>
         <p className={classes.text}>{text}</p>
       </div>
