0f9a2a84cb1f5feb3e22174b2045d084da6786cb
Dusan Stanic <dusan.stanic97@hotmail.com>
2025-03-31 16:56:30 +0200

    Update Projects Section

--- src/features/landing/page/Projects/Cards/Card/Tools/Tools.tsx
+++ src/features/landing/page/Projects/Cards/Card/Tools/Tools.tsx
@@ -1,6 +1,7 @@
 import { ITool } from "@/features/landing/page/Landing/type";
 
 import classes from "./Tools.module.scss";
+import { Button } from "@/features/components/Button/Button";
 
 interface IToolsProps {
   tools: Array<ITool>;
@@ -10,10 +11,12 @@ export const Tools = ({ tools }: IToolsProps) => {
   return (
     <div className={classes.tools}>
       {tools.map(({ name, Icon }) => (
-        <div className={classes.tool}>
-          <span>{name}</span>
-          {Icon}
-        </div>
+        <Button size="small">
+          <div className={classes.tool}>
+            <span>{name}</span>
+            {Icon}
+          </div>
+        </Button>
       ))}
       <span>...</span>
     </div>
