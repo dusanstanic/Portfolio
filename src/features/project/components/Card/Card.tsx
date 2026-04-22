30e2de02380f0e365f99e76523550c626b91e19b
Dusan Stanic <dusan.stanic97@hotmail.com>
2026-04-22 21:17:31 +0200

    Add accessibility

--- src/features/project/components/Card/Card.tsx
+++ src/features/project/components/Card/Card.tsx
@@ -1,36 +1,31 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
-import { useNavigate } from 'react-router';
-
 import { ROUTES } from '@/routes/routes';
-
-import { Button } from '@/components/Button/Button';
 
 import { Project } from '@/features/project/api/type';
 
 import { Pills } from '@/components/Pills/Pills';
 
 import classes from './Card.module.scss';
+import { Link } from '@/components/Link/Link';
 
 export const Card = ({ id, title, description, tools }: Project) => {
-  const navigate = useNavigate();
-
   return (
     <article className={classes.card}>
       <h3>{title}</h3>
       <p className={classes.description}>{description}</p>
       <Pills pills={tools} displayCount={4} />
-      <Button
+      <Link
         style={{ justifySelf: 'start' }}
-        onClick={() => navigate(ROUTES.projectId.replace(':id', id.toString()))}
+        to={ROUTES.projectId.replace(':id', id.toString())}
         className={classes.button}
       >
         <>
-          <span>Details</span>
+          Details
           <FontAwesomeIcon icon={faArrowRight} />
         </>
-      </Button>
+      </Link>
     </article>
   );
 };
